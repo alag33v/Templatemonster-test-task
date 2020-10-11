9ff1ab149ec3477c116b2802271cbb7094813b3a
alageev <alageev5610@gmail.com>
2020-10-11 22:51:15 +0300

    Added lazy load

--- js/app.js
+++ js/app.js
@@ -146,4 +146,43 @@ $(document).ready(function () {
       1000
     );
   });
+
+  // Lazy load
+  //============= dynamic placeholder for images
+
+  const placeholder =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
+  const targets = document.querySelectorAll("[data-src]");
+  targets.forEach((target) => {
+    target.src = placeholder;
+  });
+
+  //============= IntersectionObserver
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.05
+  };
+
+  const loadImage = function (entries, observer) {
+    entries.forEach((entry) => {
+      if (
+        entry.isIntersecting &&
+        entry.target.parentNode.classList.contains("loading")
+      ) {
+        entry.target.src = entry.target.getAttribute("data-src");
+        entry.target.onload = () => {
+          entry.target.parentNode.classList.remove("loading");
+          entry.target.removeAttribute("data-src");
+        };
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(loadImage, options);
+  targets.forEach((target) => {
+    observer.observe(target);
+  });
+
 });
